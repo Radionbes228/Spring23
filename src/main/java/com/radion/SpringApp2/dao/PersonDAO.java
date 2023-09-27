5f4899f2831519d71a3a53a57e6780217840cdf1
Rengoku Kyōjurō <radionbes@gmail.com>
2023-09-27 22:29:15 +0300

    CRUD Application

---
+++ src/main/java/com/radion/SpringApp2/dao/PersonDAO.java
@@ -0,0 +1,27 @@
+package com.radion.SpringApp2.dao;
+
+import com.radion.SpringApp2.models.Person;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int COUNT = 1;
+    List<Person> peopleList;
+    {
+        peopleList = new ArrayList<>();
+        peopleList.add(new Person(COUNT++, "Sergey"));
+        peopleList.add(new Person(COUNT++, "Angel"));
+        peopleList.add(new Person(COUNT++, "Phone"));
+    }
+
+    public List<Person> getPeopleList(){
+        return peopleList;
+    }
+    public Person getOnePeople(int id){
+       return peopleList.stream().filter(people -> people.getID() == id).findAny().orElse(null);
+    }
+}
---
+++ src/main/java/com/radion/SpringApp2/models/Person.java
@@ -0,0 +1,27 @@
+package com.radion.SpringApp2.models;
+
+public class Person {
+    private int ID;
+    private String name;
+
+    public Person(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
